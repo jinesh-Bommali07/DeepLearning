18972a9964084f91052a187e5e1a407385bcdf79
unknown <jinesh.bommali004@gmail.com>
2024-12-03 21:03:50 +0530

    initial commit

--- about.py
+++ about.py
@@ -1,19 +1,20 @@
 import streamlit as st
 
-# Set the title for the contact section
-st.title("Contact Information")
+def aboutpage():
+    # Set the title for the contact section
+    st.title("Contact Information")
 
-# Display contact details
-st.subheader("📞 Phone Number")
-st.write("+91 8108301759")  # Replace with your mobile number
+    # Display contact details
+    st.subheader("📞 Phone Number")
+    st.write("+91 8108301759")  # Replace with your mobile number
 
-st.subheader("📧 Email Address")
-st.write("[example@domain.com](mailto:jinesh.bommali004@gmail.com)")  # Replace with your email address
+    st.subheader("📧 Email Address")
+    st.write("[jinesh.bommali004@gmail.com](mailto:jinesh.bommali004@gmail.com)")  # Replace with your email address
 
 
-st.subheader("💻 GitHub")
-st.write("[GitHub Profile](https://github.com/jinesh-Bommali07/DeepLearning)")  # Replace with your GitHub URL
+    st.subheader("💻 GitHub")
+    st.write("[https://github.com/jinesh-Bommali07/DeepLearning](https://github.com/jinesh-Bommali07/DeepLearning)")  # Replace with your GitHub URL
 
-# Add optional styling or spacing
-st.markdown("---")
-st.write("Feel free to reach out via any of the platforms above!")
+    # Add optional styling or spacing
+    st.markdown("---")
+    st.write("Feel free to reach out via any of the platforms above!")
